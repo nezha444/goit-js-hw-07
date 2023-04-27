b8e3806f90902c5cc22fa09dece8ddc695542bcb
Nikita <bazilik2452@gmail.com>
2023-04-27 18:20:55 +0200

    .

---
+++ js/01-gallery.js
@@ -0,0 +1,31 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+// import * as basicLightbox from 'basiclightbox'
+
+// const instance = basicLightbox.create(`
+//     <img src="assets/images/image.png" width="800" height="600">
+// `)
+
+// instance.show()
+
+
+const gallery = document.querySelector('.gallery')
+const galleryItem = document.querySelectorAll('.gallery__item')
+const galleryLink = document.querySelectorAll('.gallery__link')
+const galleryImage = document.querySelectorAll('.gallery__image')
+
+
+for (let i = 0; i < galleryItems.length; i++) {
+    
+    function addValueInGallery(){
+
+
+
+    }
+    
+}
+
+
+
+console.log(galleryItems);
